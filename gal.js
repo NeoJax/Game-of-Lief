395c4d3ea8a0f085b93e1628a269aa73e96d03d8
NeoJax <Neojax@gmail.com>
2017-08-24 16:13:49 -0700

    Updating the GitHub With the work today

--- gal.js
+++ gal.js
@@ -1,3 +1,28 @@
+$(document).ready(function(){
+  $("#btn1").click(function(){
+    make_grid($("#text1").val(),$("#text2").val());
+  });
+});
+$(document).ready(function(){
+  $("#btn2").click(function(){
+    $("h1").empty();
+  });
+});
+$(document).ready(function(){
+  $('h1').delegate("click", "button.button-grid", function(){
+    alert("blah");
+  });
+});
 function make_grid(rows, columns){
-  
+  $("h1").empty();
+  for(var i = 0; i < rows; i++){
+    for(var j = 0; j < columns; j++){
+      $(document).ready(function(){
+        $("h1").append("<button class = \"button-grid\"></button>");
+      });
+    }
+    $(document).ready(function(){
+      $("h1").append("<br>");
+    });
+  }
 }
